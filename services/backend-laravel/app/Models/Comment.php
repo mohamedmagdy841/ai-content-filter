9ed780c1a1622cfdebffe2087b05ddadbd7aa6ee
Mohamed Magdy Gamal <64127744+mohamedmagdy841@users.noreply.github.com>
2025-04-08 02:26:36 +0200

    creating models

--- services/backend-laravel/app/Models/Comment.php
+++ services/backend-laravel/app/Models/Comment.php
@@ -4,9 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Comment extends Model
 {
     /** @use HasFactory<\Database\Factories\CommentFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'post_id',
+        'content',
+        'status'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function filterLogs(): MorphMany
+    {
+        return $this->morphMany(FilterLog::class, 'content');
+    }
 }
--- services/backend-laravel/app/Models/FilterLog.php
+++ services/backend-laravel/app/Models/FilterLog.php
@@ -9,4 +9,18 @@ class FilterLog extends Model
 {
     /** @use HasFactory<\Database\Factories\FilterLogFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'content_type',
+        'content_id',
+        'flagged_by',
+        'reason',
+        'confidence',
+        'status',
+    ];
+
+    public function content()
+    {
+        return $this->morphTo();
+    }
 }
