d54cb4343d269fcfffa8bcc5238fa6a0595d140d
magdy <mmagdy841@gmail.com>
2025-04-09 23:37:21 +0200

    fastapi ai models integration

--- services/backend-laravel/database/migrations/2025_04_07_220339_create_comments_table.php
+++ services/backend-laravel/database/migrations/2025_04_07_220339_create_comments_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('post_id')->constrained()->onDelete('cascade');
             $table->text('content');
-            $table->string('status')->default('under_review');
+            $table->enum('status', ['pending', 'flagged', 'deleted', 'approved'])->default('pending');
             $table->timestamps();
         });
     }
