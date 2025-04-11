b8aa7d7e9c33ed5fa4bd8a6c0e0b5ed7541ae162
magdy <mmagdy841@gmail.com>
2025-04-11 06:22:24 +0200

    resource pagination

--- services/backend-laravel/app/Http/Helpers/HttpResponse.php
+++ services/backend-laravel/app/Http/Helpers/HttpResponse.php
@@ -12,4 +12,29 @@ class HttpResponse
             'data' => $data
         ], $status);
     }
+
+    public static function paginate($data = [], $message = [], $status = 200)
+    {
+        $pagination = $data->resource->toArray();
+
+        return response()->json([
+            'status' => $status,
+            'message' => $message,
+            'data' => $pagination['data'],
+            'meta' => [
+                'current_page' => $pagination['current_page'],
+                'last_page' => $pagination['last_page'],
+                'per_page' => $pagination['per_page'],
+                'total' => $pagination['total'],
+                'next_page_url' => $pagination['next_page_url'],
+                'prev_page_url' => $pagination['prev_page_url'],
+            ],
+            'links' => [
+                'first' => $pagination['first_page_url'],
+                'last' => $pagination['last_page_url'],
+                'next' => $pagination['next_page_url'],
+                'prev' => $pagination['prev_page_url'],
+            ]
+        ], $status);
+    }
 }
