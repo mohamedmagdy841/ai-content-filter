79eaa696884f4bfefb68830ace62b0a116829369
Mohamed Magdy Gamal <64127744+mohamedmagdy841@users.noreply.github.com>
2025-04-23 15:27:03 +0200

    creating tags table

--- services/backend-laravel/app/Models/Post.php
+++ services/backend-laravel/app/Models/Post.php
@@ -65,4 +65,9 @@ class Post extends Model
     {
         return $this->morphMany(Image::class, 'imageable');
     }
+
+    public function tags()
+    {
+        return $this->morphToMany(Tag::class, 'taggable');
+    }
 }
